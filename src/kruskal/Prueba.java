a8c5290c5e442f8e1ca9282dcc7032351f79cbe1
Entrar <Entrar@10.22.146.112>
2017-01-30 11:51:27 +0000

    -Initialization- 2:B

---
+++ src/kruskal/Prueba.java
@@ -0,0 +1,15 @@
+package kruskal;
+
+/**
+ *
+ * @author Juan Francisco Pérez Caballero && Gabriel García Buey
+ */
+public class Prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+    
+}
